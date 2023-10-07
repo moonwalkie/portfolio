fe87a84e0f719fa192a02c31d8d1c48a038d51b6
moonwalkie <moonwalkietalkie@gmail.com>
2023-10-07 23:23:51 +0200

    Add href for navbar links & typewriter effect

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ function stickyNav() {
     navbar.style.cssText = "height: 4rem;";
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const h1 = document.getElementById("typewriter");
+  const text = [
+    "Hi! I'm Rafael",
+    "Welcome to my portfolio."
+  ];
+  const delay = 100; // Adjust the delay between letters
+
+  let lineIndex = 0;
+  let charIndex = 0;
+
+  function typeLetter() {
+    if (lineIndex < text.length) {
+      const line = text[lineIndex];
+      if (charIndex < line.length) {
+        if (line.charAt(charIndex) === '\n') {
+          h1.innerHTML += "<br>";
+        } else {
+          h1.innerHTML += line.charAt(charIndex);
+        }
+        charIndex++;
+        setTimeout(typeLetter, delay);
+      } else {
+        lineIndex++;
+        charIndex = 0;
+        h1.innerHTML += "<br>"; // Add line break between lines of text
+        setTimeout(typeLetter, delay);
+      }
+    }
+  }
+
+  typeLetter();
+});

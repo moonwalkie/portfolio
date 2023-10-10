0738d74ef85a3f707e2424aa7ebc7a267ba6a798
moonwalkie <moonwalkietalkie@gmail.com>
2023-10-11 00:07:18 +0200

    Add remaining images to lightbox

--- lightbox.js
+++ lightbox.js
@@ -43,6 +43,113 @@ document.addEventListener("DOMContentLoaded", function () {
         openLightbox(imageSrcArray);
     });
 
+    const box2 = document.querySelector(".box.two");
+
+    box2.addEventListener("click", function () {
+        const imageSrcArray = [
+            "./images/landscape-illustrations/AUSTRALIAN-WILDLIFE.png",
+            "./images/landscape-illustrations/MOUNTAINOUS.png",
+            "./images/landscape-illustrations/EARLY-RISER.png",
+            "./images/landscape-illustrations/WANDERING-ON-A-LOST-ISLAND.png",
+            "./images/landscape-illustrations/FRESH-AIR.png"
+        ];
+
+        openLightbox(imageSrcArray);
+    });
+
+    const box3 = document.querySelector(".box.three");
+
+    box3.addEventListener("click", function () {
+        const imageSrcArray = [
+            "./images/logos/Mona's-Beauty-LOGO-WEB-White.png",
+            "./images/logos/BESTPIESA-logo-final.png",
+            "./images/logos/BMWPARTS-logo-final.png",
+            "./images/logos/JAPANPARTS-logo-final.png",
+            "./images/logos/XSS-Logo.png",
+            "./images/logos/WEIMO-PROD-XSS-TV-2.png",
+            "./images/logos/WEIMO-PROD-XSS-TV.png"
+        ];
+
+        openLightbox(imageSrcArray);
+    });
+
+    const box4 = document.querySelector(".box.four");
+
+    box4.addEventListener("click", function () {
+        const imageSrcArray = [
+            "./images/illustrations/Lollipop-Lick-Artwork.png",
+            "./images/illustrations/3-SPERMS.png",
+            "./images/illustrations/Apple-Background.png",
+            "./images/illustrations/Red-Apple-Background.png"
+        ];
+
+        openLightbox(imageSrcArray);
+    });
+    
+    const box5 = document.querySelector(".box.five");
+
+    box5.addEventListener("click", function () {
+        const imageSrcArray = [
+            "./images/jungle-landscape-series/ADAM-AND-EVE.png",
+            "./images/jungle-landscape-series/God's-Acre-Landscape-Background-by-Moon.png",
+            "./images/jungle-landscape-series/WINTER-SEASON.png",
+            "./images/jungle-landscape-series/Dark-Jungle-Landscape-Background-by-Moon.png",
+            "./images/jungle-landscape-series/Jungle-Landscape-Background-by-Moon.png"
+        ];
+
+        openLightbox(imageSrcArray);
+    });
+
+    const box6 = document.querySelector(".box.six");
+
+    box6.addEventListener("click", function () {
+        const imageSrcArray = [
+            "./images/icons/XSS-ICONS-PREVIEW.png",
+            "./images/icons/XSS-RANKS-PREVIEW.png"
+        ];
+
+        openLightbox(imageSrcArray);
+    });
+
+    const box7 = document.querySelector(".box.seven");
+
+    box7.addEventListener("click", function () {
+        const imageSrcArray = [
+            "./images/various-backgrounds/MOON-Gioconda.png",
+            "./images/various-backgrounds/MoonWalk-KING-LION&SANTA-Background.png",
+            "./images/various-backgrounds/Windows-Background-by-Moon.png",
+            "./images/various-backgrounds/Pawn-Piece-Background-by-Moon.png",
+            "./images/various-backgrounds/Abstract-Polygon-Background-by-Moon.png"
+        ];
+
+        openLightbox(imageSrcArray);
+    });
+
+    const box8 = document.querySelector(".box.eight");
+
+    box8.addEventListener("click", function () {
+        const imageSrcArray = [
+            "./images/vector-portraits/2Pac-Artwork.png",
+            "./images/vector-portraits/Eminem-Artwork.png" 
+        ];
+
+        openLightbox(imageSrcArray);
+    });
+
+    const box9 = document.querySelector(".box.nine");
+
+    box9.addEventListener("click", function () {
+        const imageSrcArray = [
+            "./images/forum-signatures/MoonWalk-I'LL-BE-OKAY-Signature.png",
+            "./images/forum-signatures/MoonWalk-KING-LION-Signature.gif",
+            "./images/forum-signatures/MoonWalk-LOGO-Signature.png",
+            "./images/forum-signatures/MoonWalk-Gioconda-Signature.png",
+            "./images/forum-signatures/AQUARIUM-Signature.png"
+        ];
+
+        openLightbox(imageSrcArray);
+    });
+
     // Attach click event listener to the close button
     closeButton.addEventListener("click", closeLightbox);
 

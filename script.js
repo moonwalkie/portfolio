d6055e44a3df4fa565ce5153d3e198d91e1daf52
moonwalkie <moonwalkietalkie@gmail.com>
2023-10-28 15:44:59 +0200

    Add theme toggle switch between dark & light mode

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 window.onscroll = function() {stickyNav()};
 
 let navbar = document.querySelector('.container-nav');
-let logo = document.querySelector('.mw');
-let logoHover = document.querySelector('.mw.hover');
 
 // Sticky Navbar on the top
 function stickyNav() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     navbar.style.cssText = "height: 3.2rem;";
-    logo.style.cssText = "height: 2.5rem";
-    logoHover.style.cssText = "height: 2.5rem"; // Adjust the navbar and the logo when scrolling
   } else {
     navbar.style.cssText = "height: 4rem;";
-    logo.style.cssText = "height: 3rem";
-    logoHover.style.cssText = "height: 3rem";
   }
 }
 
@@ -54,6 +48,60 @@ document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     typeLetter();
   }, initialDelay);
+
+  // Theme toggle
+  const themeToggle = document.querySelector(".theme");
+  const sun = themeToggle.querySelector(".sun");
+  const moon = themeToggle.querySelector(".moon");
+  const main = document.querySelector(".main");
+  const navbar = document.querySelector(".navbar");
+  const info = document.querySelector(".info");
+  const aboutMeImg = document.querySelector(".about-me-img");
+  const hr = document.querySelector("hr");
+  const lightbox = document.querySelector(".lightbox-content");
+  const navLinks = document.querySelector(".nav-links");
+
+  let isMoonClicked = false;
+
+  // Function to switch the theme
+  function switchTheme() {
+  if (isMoonClicked) {
+    // Light Mode
+    main.style.background = "linear-gradient(#c2c2c2, #e3e3e3, #c2c2c2)";
+    navbar.style.cssText = "background-color: #c2c2c2; box-shadow: 0 0 0.5rem #757575";
+    info.style.background = "linear-gradient(#e3e3e3, #c2c2c2)";
+    aboutMeImg.style.cssText = "box-shadow: 3rem 3rem 3rem #bababa";
+    hr.style.cssText = "background: -webkit-gradient(linear, 0 0, 100% 0, from(transparent), to(transparent), color-stop(50%, #858e9e))";
+    h1.style.color = "#858e9e";
+    lightbox.style.background = "#c2c2c2";
+    navLinks.classList.add("light-mode-color");
+    navLinks.classList.add("light-mode-shadow");
+    moon.style.opacity = 0;
+    sun.style.opacity = 1;
+  } else {
+    // Dark Mode
+    main.style.background = "linear-gradient(#0d0d0e, #18181a, #0d0d0e)";
+    navbar.style.cssText = "background-color: #141518; box-shadow: 0 0 0.5rem #0c0c0e";
+    info.style.background = "linear-gradient(#141518, #0b0c0e)";
+    aboutMeImg.style.cssText = "box-shadow: 3rem 3rem 3rem #0b0c0e";
+    hr.style.cssText = "background: -webkit-gradient(linear, 0 0, 100% 0, from(transparent), to(transparent), color-stop(50%, #f5f0dc))";
+    h1.style.color = "#f5f0dc";
+    lightbox.style.background = "#141518";
+    navLinks.classList.remove("light-mode-color");
+    navLinks.classList.remove("light-mode-shadow");
+    moon.style.opacity = 1;
+    sun.style.opacity = 0;
+  }
+  }
+
+  // Apply initial theme
+  switchTheme();
+
+  // Toggle the theme on click
+  themeToggle.addEventListener("click", function () {
+  isMoonClicked = !isMoonClicked;
+  switchTheme();
+  });
 });
 
 // Toggle the hamburger menu
